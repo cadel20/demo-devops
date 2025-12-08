cf73d1bd3b655ec5dbbae83cb4916d51d34a7f05
cadel20 <thaumy20@gmail.com>
2025-12-08 10:19:28 +0100

    sss3

--- infrastructure/fournisseurs.tf
+++ infrastructure/fournisseurs.tf
@@ -1,34 +1,19 @@
-# Configuration Terraform avec Docker
+# Configuration minimale et sécurisée
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.0.0"
   
   required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.4"
-    }
-    
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5"
+      version = "~> 3.5.1"
     }
     
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4.0"
     }
   }
-  
-  backend "local" {
-    path = "terraform.tfstate"
-  }
 }
 
-# Provider Docker avec détection automatique du système
-provider "docker" {
-  # Détection automatique pour Windows/Linux/Mac
-  # Terraform choisira le bon host selon votre OS
-}
-
-provider "local" {}
 provider "random" {}
+provider "local" {}
