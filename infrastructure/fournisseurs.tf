e5a3472d106a32cce9b2eed78af8fbffba6d9ebf
cadel20 <thaumy20@gmail.com>
2025-12-08 10:35:38 +0100

    Update fournisseurs.tf

--- infrastructure/fournisseurs.tf
+++ infrastructure/fournisseurs.tf
@@ -1,19 +1,25 @@
-# Configuration minimale et sécurisée
+# Configuration avec les versions DÉJÀ installées sur votre système
 terraform {
   required_version = ">= 1.0.0"
   
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "3.7.2"  # Version déjà installée sur votre système
     }
     
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.4.0"
+      version = "2.6.1"  # Version déjà installée sur votre système
+    }
+    
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"  # Version déjà installée
     }
   }
 }
 
 provider "random" {}
 provider "local" {}
+provider "null" {}
