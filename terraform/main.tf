a286091c324274c32ca3432a021bc93560c36fe1
cadel20 <cadel20@users.noreply.github.com>
2025-12-08 11:32:40 +0000

    deploy: cf7db29003734391f4d422b875e5e11a502d994a

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "docker" {}
 
 resource "docker_image" "app" {
-  name         = "cadel2/demo-devops:latest"
+  name         = "cadel2/demo-devops-app:latest"
   keep_locally = true
 }
 
